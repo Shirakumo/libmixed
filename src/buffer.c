4968e30389e6d478733c360937ffd4b58d229fc6
Till Ehrengruber <till@ehrengruber.ch>
2017-05-18 19:33:09 +0200

    Optimized mixed_buffer_to_channel

--- src/buffer.c
+++ src/buffer.c
@@ -18,7 +18,7 @@ MIXED_EXPORT void mixed_free_buffer(struct mixed_buffer *buffer){
 }
 
 #define DEF_MIXED_TRANSFER_SAMPLE_FROM(name, datatype) \
-inline void mixed_transfer_sample_from_##name(struct mixed_channel *in, size_t is, struct mixed_buffer *out, size_t os) { \
+static inline void mixed_transfer_sample_from_##name(struct mixed_channel *in, size_t is, struct mixed_buffer *out, size_t os) { \
   out->data[os] = mixed_from_##name(((datatype *)in->data)[is]); \
 }
 
@@ -50,7 +50,7 @@ extern inline void mixed_transfer_sample_from_uint24(struct mixed_channel *in, s
 }
 
 #define DEF_MIXED_BUFFER_FROM_CHANNEL(name) \
-MIXED_EXPORT int mixed_buffer_from_##name##_channel(struct mixed_channel *in, struct mixed_buffer **outs, size_t samples) { \
+void mixed_buffer_from_##name##_channel(struct mixed_channel *in, struct mixed_buffer **outs, size_t samples) { \
   mixed_err(MIXED_NO_ERROR); \
   size_t channels = in->channels; \
   switch(in->layout){ \
@@ -77,7 +77,6 @@ MIXED_EXPORT int mixed_buffer_from_##name##_channel(struct mixed_channel *in, st
     mixed_err(MIXED_UNKNOWN_LAYOUT); \
     break; \
   } \
-  return (mixed_error() == MIXED_NO_ERROR); \
 }
 
 DEF_MIXED_BUFFER_FROM_CHANNEL(int8);
@@ -127,80 +126,113 @@ MIXED_EXPORT int mixed_buffer_from_channel(struct mixed_channel *in, struct mixe
     mixed_err(MIXED_UNKNOWN_ENCODING);
     break;
   }
+  return (mixed_error() == MIXED_NO_ERROR);
+}
+
+#define DEF_MIXED_TRANSFER_SAMPLE_TO(name, datatype) \
+static inline void mixed_transfer_sample_to_##name(struct mixed_buffer *in, size_t is, struct mixed_channel *out, size_t os){ \
+  ((datatype *)out->data)[os] = mixed_to_##name(in->data[is]); \
 }
 
 // We assume little endian for all formats.
-extern inline void mixed_transfer_sample_to(struct mixed_buffer *in, size_t is, struct mixed_channel *out, size_t os){
+DEF_MIXED_TRANSFER_SAMPLE_TO(int8, int8_t);
+DEF_MIXED_TRANSFER_SAMPLE_TO(uint8, uint8_t);
+DEF_MIXED_TRANSFER_SAMPLE_TO(int16, int16_t);
+DEF_MIXED_TRANSFER_SAMPLE_TO(uint16, uint16_t);
+DEF_MIXED_TRANSFER_SAMPLE_TO(int32, int32_t);
+DEF_MIXED_TRANSFER_SAMPLE_TO(uint32, uint32_t);
+DEF_MIXED_TRANSFER_SAMPLE_TO(float, float);
+DEF_MIXED_TRANSFER_SAMPLE_TO(double, double);
+
+extern inline void mixed_transfer_sample_to_int24(struct mixed_buffer *in, size_t is, struct mixed_channel *out, size_t os){
+  int24_t sample = mixed_to_int24(in->data[is]);
+  ((uint8_t *)out->data)[os+2] = (sample >> 16) & 0xFF;
+  ((uint8_t *)out->data)[os+1] = (sample >>  8) & 0xFF;
+  ((uint8_t *)out->data)[os+0] = (sample >>  0) & 0xFF;
+}
+
+extern inline void mixed_transfer_sample_to_uint24(struct mixed_buffer *in, size_t is, struct mixed_channel *out, size_t os){
+  uint24_t sample = mixed_to_uint24(in->data[is]);
+  ((uint8_t *)out->data)[os+2] = (sample >> 16) & 0xFF;
+  ((uint8_t *)out->data)[os+1] = (sample >>  8) & 0xFF;
+  ((uint8_t *)out->data)[os+0] = (sample >>  0) & 0xFF;
+}
+
+#define DEF_MIXED_BUFFER_TO_CHANNEL(name) \
+static inline void mixed_buffer_to_##name##_channel(struct mixed_buffer **ins, struct mixed_channel *out, size_t samples){  \
+  mixed_err(MIXED_NO_ERROR);  \
+  size_t channels = out->channels;  \
+  switch(out->layout){  \
+  case MIXED_ALTERNATING:{  \
+    for(size_t channel=0; channel<channels; ++channel){  \
+      struct mixed_buffer *in = ins[channel];  \
+      for(size_t sample=0; sample<samples; ++sample){  \
+        mixed_transfer_sample_to_##name(in, sample, out, sample*channels+channel);  \
+      }  \
+    }}  \
+    break;  \
+  case MIXED_SEQUENTIAL:{  \
+    size_t offset = 0;  \
+    for(uint8_t channel=0; channel<channels; ++channel){  \
+      struct mixed_buffer *in = ins[channel];  \
+      for(size_t sample=0; sample<samples; ++sample){  \
+        mixed_transfer_sample_to_##name(in, sample, out, sample+offset);  \
+        ++sample;  \
+      }  \
+      offset += samples;  \
+    }}  \
+    break;  \
+  default:  \
+    mixed_err(MIXED_UNKNOWN_LAYOUT);  \
+    break;  \
+  }  \
+}
+
+DEF_MIXED_BUFFER_TO_CHANNEL(int8);
+DEF_MIXED_BUFFER_TO_CHANNEL(uint8);
+DEF_MIXED_BUFFER_TO_CHANNEL(int16);
+DEF_MIXED_BUFFER_TO_CHANNEL(uint16);
+DEF_MIXED_BUFFER_TO_CHANNEL(int24);
+DEF_MIXED_BUFFER_TO_CHANNEL(uint24);
+DEF_MIXED_BUFFER_TO_CHANNEL(int32);
+DEF_MIXED_BUFFER_TO_CHANNEL(uint32);
+DEF_MIXED_BUFFER_TO_CHANNEL(float);
+DEF_MIXED_BUFFER_TO_CHANNEL(double);
+
+MIXED_EXPORT int mixed_buffer_to_channel(struct mixed_buffer **ins, struct mixed_channel *out, size_t samples){
   switch(out->encoding){
   case MIXED_INT8:
-    ((int8_t *)out->data)[os] = mixed_to_int8(in->data[is]);
+    mixed_buffer_to_int8_channel(ins, out, samples);
     break;
   case MIXED_UINT8:
-    ((uint8_t *)out->data)[os] = mixed_to_uint8(in->data[is]);
+    mixed_buffer_to_uint8_channel(ins, out, samples);
     break;
   case MIXED_INT16:
-    ((int16_t *)out->data)[os] = mixed_to_int16(in->data[is]);
+    mixed_buffer_to_int16_channel(ins, out, samples);
     break;
   case MIXED_UINT16:
-    ((uint16_t *)out->data)[os] = mixed_to_uint16(in->data[is]);
+    mixed_buffer_to_uint16_channel(ins, out, samples);
     break;
-  case MIXED_INT24:{
-    int24_t sample = mixed_to_int24(in->data[is]);
-    ((uint8_t *)out->data)[os+2] = (sample >> 16) & 0xFF;
-    ((uint8_t *)out->data)[os+1] = (sample >>  8) & 0xFF;
-    ((uint8_t *)out->data)[os+0] = (sample >>  0) & 0xFF;
-  }
+  case MIXED_INT24:
+    mixed_buffer_to_int24_channel(ins, out, samples);
     break;
-  case MIXED_UINT24:{
-    uint24_t sample = mixed_to_uint24(in->data[is]);
-    ((uint8_t *)out->data)[os+2] = (sample >> 16) & 0xFF;
-    ((uint8_t *)out->data)[os+1] = (sample >>  8) & 0xFF;
-    ((uint8_t *)out->data)[os+0] = (sample >>  0) & 0xFF;
-  }
+  case MIXED_UINT24:
+    mixed_buffer_to_uint24_channel(ins, out, samples);
     break;
   case MIXED_INT32:
-    ((int32_t *)out->data)[os] = mixed_to_int32(in->data[is]);
+    mixed_buffer_to_int32_channel(ins, out, samples);
     break;
   case MIXED_UINT32:
-    ((uint32_t *)out->data)[os] = mixed_to_uint32(in->data[is]);
+    mixed_buffer_to_uint32_channel(ins, out, samples);
     break;
   case MIXED_FLOAT:
-    ((float *)out->data)[os] = mixed_to_float(in->data[is]);
+    mixed_buffer_to_float_channel(ins, out, samples);
     break;
   case MIXED_DOUBLE:
-    ((double *)out->data)[os] = mixed_to_double(in->data[is]);
+    mixed_buffer_to_double_channel(ins, out, samples);
     break;
   default:
     mixed_err(MIXED_UNKNOWN_ENCODING);
-    break;
-  }
-}
-
-MIXED_EXPORT int mixed_buffer_to_channel(struct mixed_buffer **ins, struct mixed_channel *out, size_t samples){
-  mixed_err(MIXED_NO_ERROR);
-  size_t channels = out->channels;
-  switch(out->layout){
-  case MIXED_ALTERNATING:{
-    for(size_t channel=0; channel<channels; ++channel){
-      struct mixed_buffer *in = ins[channel];
-      for(size_t sample=0; sample<samples; ++sample){
-        mixed_transfer_sample_to(in, sample, out, sample*channels+channel);
-      }
-    }}
-    break;
-  case MIXED_SEQUENTIAL:{
-    size_t offset = 0;
-    for(uint8_t channel=0; channel<channels; ++channel){
-      struct mixed_buffer *in = ins[channel];
-      for(size_t sample=0; sample<samples; ++sample){
-        mixed_transfer_sample_to(in, sample, out, sample+offset);
-        ++sample;
-      }
-      offset += samples;
-    }}
-    break;
-  default:
-    mixed_err(MIXED_UNKNOWN_LAYOUT);
     break;
   }
   return (mixed_error() == MIXED_NO_ERROR);
