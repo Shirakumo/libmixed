d07323c3112857678ae217237bee9068639dfa01
Shinmera <shinmera@tymoon.eu>
2020-07-11 17:53:32 +0200

    Minor

--- test/packer.c
+++ test/packer.c
@@ -23,8 +23,8 @@ define_test(single_channel_no_resample, {
     pass(make_pack(MIXED_INT8, 1, &pack_i));
     pass(make_pack(MIXED_INT8, 1, &pack_o));
     pass(mixed_make_buffer(pack_i.frames, &buffer));
-    pass(mixed_make_segment_packer(&pack_i, pack_o.samplerate, &packer));
-    pass(mixed_make_segment_unpacker(&pack_o, pack_i.samplerate, &unpacker));
+    pass(mixed_make_segment_unpacker(&pack_i, pack_i.samplerate, &unpacker));
+    pass(mixed_make_segment_packer(&pack_o, pack_o.samplerate, &packer));
     // Connect the buffers
     pass(mixed_segment_set_out(MIXED_BUFFER, MIXED_MONO, &buffer, &unpacker));
     pass(mixed_segment_set_in(MIXED_BUFFER, MIXED_MONO, &buffer, &packer));
@@ -39,7 +39,7 @@ define_test(single_channel_no_resample, {
     // Check
     is(pack_o.frames, pack_i.frames);
     for(int i=0; i<pack_i.size; ++i)
-      is(data_i[i], data_o[i]);
+      is(data_o[i], data_i[i]);
 
   cleanup:
     mixed_free_segment(&packer);
@@ -49,18 +49,18 @@ define_test(single_channel_no_resample, {
     if(pack_o.data) free(pack_o.data);
   })
 
-define_test(single_channel_resample, {
+define_test(single_channel_resample_out, {
     struct mixed_packed_audio pack_i = {0};
     struct mixed_packed_audio pack_o = {0};
     struct mixed_buffer buffer = {0};
-    struct mixed_segment packer = {0};
     struct mixed_segment unpacker = {0};
+    struct mixed_segment packer = {0};
     // Allocate stuff
     pass(make_pack(MIXED_INT8, 1, &pack_i));
     pass(make_pack(MIXED_INT8, 1, &pack_o));
-    pass(mixed_make_buffer(16, &buffer));
-    pass(mixed_make_segment_packer(&pack_i, 75, &packer));
-    pass(mixed_make_segment_unpacker(&pack_o, 75, &unpacker));
+    pass(mixed_make_buffer(pack_i.frames, &buffer));
+    pass(mixed_make_segment_unpacker(&pack_i, 100, &unpacker));
+    pass(mixed_make_segment_packer(&pack_o, 80, &packer));
     // Connect the buffers
     pass(mixed_segment_set_out(MIXED_BUFFER, MIXED_MONO, &buffer, &unpacker));
     pass(mixed_segment_set_in(MIXED_BUFFER, MIXED_MONO, &buffer, &packer));
@@ -72,10 +72,44 @@ define_test(single_channel_resample, {
     // Run
     pass(mixed_segment_mix(&unpacker));
     pass(mixed_segment_mix(&packer));
-    // Check
-    is(pack_i.frames, pack_o.frames);
+    // Check for expected
+    is(pack_o.frames, 60);
+
+  cleanup:
+    mixed_free_segment(&packer);
+    mixed_free_segment(&unpacker);
+    mixed_free_buffer(&buffer);
+    if(pack_i.data) free(pack_i.data);
+    if(pack_o.data) free(pack_o.data);
+  })
+
+define_test(single_channel_resample_in_out, {
+    struct mixed_packed_audio pack_i = {0};
+    struct mixed_packed_audio pack_o = {0};
+    struct mixed_buffer buffer = {0};
+    struct mixed_segment unpacker = {0};
+    struct mixed_segment packer = {0};
+    // Allocate stuff
+    pass(make_pack(MIXED_INT8, 1, &pack_i));
+    pass(make_pack(MIXED_INT8, 1, &pack_o));
+    pass(mixed_make_buffer(pack_i.frames, &buffer));
+    pass(mixed_make_segment_unpacker(&pack_i, 80, &unpacker));
+    pass(mixed_make_segment_packer(&pack_o, 80, &packer));
+    // Connect the buffers
+    pass(mixed_segment_set_out(MIXED_BUFFER, MIXED_MONO, &buffer, &unpacker));
+    pass(mixed_segment_set_in(MIXED_BUFFER, MIXED_MONO, &buffer, &packer));
+    // Fill data
+    int8_t *data_i = (int8_t *)pack_i.data;
+    int8_t *data_o = (int8_t *)pack_o.data;
     for(int i=0; i<pack_i.size; ++i)
-      is(data_i[i], data_o[i]);
+      data_i[i] = rand()%128;
+    // Run
+    pass(mixed_segment_mix(&unpacker));
+    pass(mixed_segment_mix(&packer));
+    // Check for expected
+    is(pack_o.frames, 60);
+    for(int i=0; i<pack_o.size; ++i)
+      is(data_o[i], data_i[i]);
 
   cleanup:
     mixed_free_segment(&packer);
