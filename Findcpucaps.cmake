b70f5c4f3742531289027300b80f68120fae0b99
Shinmera <shinmera@tymoon.eu>
2020-07-24 15:43:56 +0200

    Attempt to be smarter about CPU extension flags.

---
+++ Findcpucaps.cmake
@@ -0,0 +1,29 @@
+include(CheckCSourceRuns)
+
+function(check_flag var macroname flag)
+  if(NOT DEFINED ${var})
+    message(STATUS "Checking for ${macroname}")
+    set(test_file "${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp/${macroname}.c")
+    file(WRITE ${test_file} "
+void main(){
+#ifndef ${macroname}
+#error \"No support\"
+#endif
+}")
+    try_compile(${var} "${CMAKE_BINARY_DIR}/CMakeFiles/CMakeTmp" ${test_file})
+    if(${var})
+      message(STATUS "Checking for ${macroname} - found")
+    endif()
+  endif()
+  if(${var})
+    set(CPU_EXTENSION_FLAGS ${flag} ${CPU_EXTENSION_FLAGS} PARENT_SCOPE)
+  endif()
+endfunction()
+
+set(CPU_EXTENSION_FLAGS "-march=native")
+check_flag(HAS_SSE "__SSE__" "-msse")
+check_flag(HAS_SSE2 "__SSE2__" "-msse2")
+check_flag(HAS_SSE3 "__SSE3__" "-msse3")
+check_flag(HAS_SSE4 "__SSE4__" "-msse4")
+check_flag(HAS_AVX "__AVX__" "-mavx")
+check_flag(HAS_AVX2 "__AVX2__" "-mavx2")
